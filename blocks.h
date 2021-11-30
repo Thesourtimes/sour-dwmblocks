86ab58aff222ddc6adc411328df8d706fe48e060
Thesourtimes <cckhmck@gmail.com>
2021-11-30 20:15:07 +0300

    Init commit

--- blocks.h
+++ blocks.h
@@ -2,10 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"Charge:", "cat /sys/class/power_supply/BAT0/capacity", 30,	0},
+	
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 
-	{"t", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
-
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
