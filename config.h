339595a19a767d634edc08076310ad1251de3491
Thesourtimes <cckhmck@gmail.com>
2022-05-27 02:06:59 +0300

    blocks.h -> config.h

--- config.h
+++ config.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"[ ", "free -h | awk '/^Swap/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+        {"[ ", "whoami", 0, 0},
+        {" ", "free -h | awk '/^Swap/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
 	{" ", "sb-cpu", 10, 0},
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 10, 0},
 	{" ", "cat /sys/class/power_supply/BAT0/capacity", 30,	0},
-	{" ", "date '+%b %d (%a) %I:%M%p]'",					5,		0},
+	{" ", "date '+%b %d (%a) %I:%M%p]'", 5, 0},
 
 };
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
